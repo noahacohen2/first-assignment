e95380e36749ea3cf4648ca214d0469cd990b644
Daniel Sabag <daniel108s17b@gmail.com>
2022-11-26 15:46:09 +0200

    folders arrangement

---
+++ src/components/Item/Item.js
@@ -0,0 +1,16 @@
+import "./Item.css";
+import Card from "./Card/Card";
+
+function Item(props) {
+  return (
+    <Card className="item-card">
+      <div className="item-basic-data">
+        <div className="item-name">{props.name}</div>
+        <div className="item-price">{props.price}</div>
+      </div>
+      <img className="item-img" src={props.img} />
+    </Card>
+  );
+}
+
+export default Item;
---
+++ src/components/Items/Items.js
@@ -0,0 +1,14 @@
+import Item from "../Item/Item";
+import "./Items.css";
+
+function Items(props) {
+  return (
+    <div className="all-items">
+      {props.items.map((item) => (
+        <Item name={item.name} price={item.price} img={item.img}></Item>
+      ))}
+    </div>
+  );
+}
+
+export default Items;
