6706e09d94beb78295b105559f07ea743073185d
Daniel Sabag <daniel108s17b@gmail.com>
2022-12-11 21:27:30 +0200

    add css to user form

--- client/src/components/UserForm/UserForm.js
+++ client/src/components/UserForm/UserForm.js
@@ -1,45 +1,47 @@
-import './UserForm.css'
-import { useContext } from 'react';
+import "./UserForm.css";
+import { useContext } from "react";
 import AppContext from "../../context";
 
-
-// import React, { useEffect, useState } from 'react';
 const UserForm = (props) => {
+  const [userData, setUserDate] = useContext(AppContext).userData;
 
-    const [userData, setUserDate] = useContext(AppContext).userData;
+  const nameChangeHandeler = (event) => {
+    let currUserData = userData;
+    currUserData.UserName = event.target.value;
+    setUserDate(currUserData);
+    // props.onUserNameChange(event.target.value);
+  };
+  const idChangeHandeler = (event) => {
+    let currUserData = userData;
+    console.log(event.target.value);
+    currUserData.UserId = event.target.value;
+    setUserDate(currUserData);
+    // props.onUserIdChange(event.target.value);
+  };
 
-    const nameChangeHandeler = (event) => {
-        let currUserData = userData;
-        currUserData.UserName = event.target.value;
-        setUserDate(currUserData)
-        // props.onUserNameChange(event.target.value);
-    }
-    const idChangeHandeler = (event) => {
-        let currUserData = userData;
-        console.log(event.target.value);
-        currUserData.UserId = event.target.value;
-        setUserDate(currUserData)
-        // props.onUserIdChange(event.target.value);
-
-    }
-
-    return (
-        <>
-            <div className="new-user__control">
-                <div className="new-user__control">
-                    <label>User name</label>
-                    <input type='text' value={userData.UserName} onChange={nameChangeHandeler} />
-                </div>
-                <div className="new-user__control">
-                    <label>User id</label>
-                    <input type='number' min="0" value={userData.UserId} onChange={idChangeHandeler} />
-                </div>
-            </div>
-        </>
-    );
-
-
-
-}
+  return (
+    <>
+      <div className="user-form">
+        <label>User name:</label>
+        <input
+          className="form-input"
+          type="text"
+          value={userData.UserName}
+          onChange={nameChangeHandeler}
+        />
+      </div>
+      <div className="user-form">
+        <label>User id:</label>
+        <input
+          type="number"
+          min="0"
+          value={userData.UserId}
+          onChange={idChangeHandeler}
+          className="form-input"
+        />
+      </div>
+    </>
+  );
+};
 
 export default UserForm;
